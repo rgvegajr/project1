6f5cfb3032b1b66fd2ae950fe5f63ca395059e2c
Ramon Vega <32487850+rgvegajr@users.noreply.github.com>
2019-11-06 21:35:02 -0500

    latest app.js
    latest app.js includes submit, yelp api ajax query, firebase storage, and table build.  Needs map functionality.  Would like to include a database clear function but still needs research.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,8 +16,8 @@ $(document).ready(function() {
     // Create a variable to reference the database function
     let database = firebase.database();
 
-    let item = "";
-    let zip = "";
+    let item = "hats";
+    let zip = "33172";
     let store = "";
     let radius = 10000;
     let storeNum = 5;
@@ -32,8 +32,8 @@ $(document).ready(function() {
         event.preventDefault();
         item = $("#searchItem").val().trim();
         zip = $("#zipCode").val().trim();
-        // radius = $("#radius-input").val().trim();
-        // storeNum = $("#storeNum-input").val().trim();
+        radius = Math.round(($("#radius").val().trim() * 1609.34));
+        storeNum = $("#storeNum").val().trim();
         console.log(item);
         console.log(zip);
         console.log(radius);
@@ -43,12 +43,7 @@ $(document).ready(function() {
 
         let apikey = "q1OFvftbUw9yLkYlGeg8md7bDcT0Z35v9n_DP_qQFjUzHcLHKJ87k3b7MWe35-4NNrGQ_gPOQm9WBXuTl785tf8a55k3sRNL_ItoMCZu1jkyXLWNs0OI_NydhyK6XXYx";
         let queryurl = "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=" + item + "&location=" + zip + "&radius=" + radius + "&limit=" + storeNum;
-        // let lat = [];
-        // let long = [];
-        // let names = [];
-        // let address = [];
-        // let phone_number = [];
-        // let distance = [];
+
 
         $.ajax({
             url: queryurl,
@@ -63,12 +58,7 @@ $(document).ready(function() {
             console.log(base)
 
             for (let i = 0; i < base.length; i++) {
-                // lat.push(base[i].coordinates.latitude);
-                // long.push(base[i].coordinates.longitude);
-                // names.push(base[i].name);
-                // address.push(base[i].location.display_address);
-                // phone_number.push(base[i].display_phone);
-                // distance.push(base[i].distance + " Meters");
+
                 storeName = base[i].name;
                 lat = base[i].coordinates.latitude;
                 long = base[i].coordinates.longitude;
@@ -83,37 +73,18 @@ $(document).ready(function() {
                     storeName: storeName,
                     storeAdd: storeAdd,
                     storeDist: storeDist,
+                    lat: lat,
+                    long: long,
                     storePhone: storePhone,
                     dateAdded: firebase.database.ServerValue.TIMESTAMP
 
                 });
 
 
-                // database.ref().push({ base: base });
-
-                // for (let i = 0; i < storeNum; i++) {
-                //     const storeName = results[i].name;
-                //     const storeAdd = results[i].location.display_address;
-                //     const storeDist = results[i].distance;
-                //     const storePhone = results[i].phone;
-                //     database.ref().push({
-                //         item: item,
-                //         zip: zip,
-                //         radius: raidus,
-                //         storeNum: storeNum,
-                //         storeName: storeName,
-                //         storeAdd: storeAdd,
-                //         storeDist: storeDist,
-                //         storePhone: storePhone,
-                //         dateAdded: firebase.database.ServerValue.TIMESTAMP
-                //     });
-                // };
             };
         });
 
-
-
-        $("#table").empty();
+        // $("#table").empty();
         database.ref().on("child_added", function(childSnapshot) {
             console.log("db call for table=" + childSnapshot.val());
             let newStore = $("<tr>").append(
@@ -121,15 +92,34 @@ $(document).ready(function() {
                 $("<td>").text(childSnapshot.val().storeName),
                 $("<td>").text(childSnapshot.val().storeAdd),
                 $("<td>").text(Math.round((childSnapshot.val().storeDist) / 1609.344)),
-                $("<td>").text(childSnapshot.val().storePhone),
-                $("<td>").text("Show on map"),
-                $("<td>").text("Favorite")
-
+                $("<td>").text(childSnapshot.val().storePhone)
             );
             $("#table").append(newStore);
+
         }, function(errorObject) {
             console.log("Errors handled: " + errorObject.code);
         });
+
+        L.mapquest.key = 'lYrP4vF3Uk5zgTiGGuEzQGwGIVDGuy24';
+
+        let map = L.mapquest.map('map', {
+            center: [25.74, -80.29],
+            layers: L.mapquest.tileLayer('map'),
+            zoom: 12
+        });
+
+        L.mapquest.textMarker([childSnapshot.val().lat, childSnapshot.val().long], {
+            text: childSnapshot.val().storeName,
+            subtext: childSnapshot.val().storeAdd,
+            draggable: false,
+            position: 'right',
+            type: 'marker',
+            icon: {
+                primaryColor: '#333333',
+                secondaryColor: '#333333',
+                size: 'sm'
+            }
+        }).addTo(map);
 
     });
 
